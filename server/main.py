118d0c4ab988bbcf3d77c6ee8a2ddf351f510a31
VukIG <vuk.ignjatovic05@gmail.com>
2024-02-19 12:43:15 +0100

    Fixing the bug for the /predict route

--- server/main.py
+++ server/main.py
@@ -1,9 +1,12 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from PIL import Image
 import numpy as np
 import io
 import base64
+
+from pydantic import BaseModel
 #pip installation required + add the version to requirements.txt
 
 app = FastAPI()
@@ -18,20 +21,31 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class FormData:
-    img:str
-    
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+from PIL import Image
+import numpy as np
+import io
+import base64
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class FormData(BaseModel):
+    img: Optional[str] = None
+
 def base64_to_numpy(base64_string):
     decoded_bytes = base64.b64decode(base64_string)
     image = Image.open(io.BytesIO(decoded_bytes))
     image_np = np.array(image)
     return image_np
-
-# Load your cancer detection model only once when the app starts
-@app.get('/')
-def index():
-    return {'message': 'Amenelibockura'}
-
 
 @app.post('/predict')
 def predict(formData: FormData):
