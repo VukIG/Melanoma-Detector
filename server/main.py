37ff6fe67a9f45d5f058fa34fd7f0de9dd59e32f
Rich <horich21@gmail.com>
2024-03-06 17:30:58 -0800

    Updated server file

--- server/main.py
+++ server/main.py
@@ -1,12 +1,12 @@
 import binascii
+from fastapi import Request
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, File, Form, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from PIL import Image
 import numpy as np
 import io
 import base64
-
 from pydantic import BaseModel
 #pip installation required + add the version to requirements.txt
 
@@ -22,22 +22,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-def base64_to_numpy(base64_string):
-    try:
-        imgdata = base64_string.split(',')[1]
-        padding_needed = len(imgdata) %   4
-        if padding_needed >   0:
-            imgdata += '=' * (4 - padding_needed)
-        decoded = base64.b64decode(imgdata)
-        image = Image.open(io.BytesIO(decoded))
-        image_np = np.array(image)
-        return image_np
-    except (binascii.Error, IOError) as e:
-        print(f"Error processing image: {e}")
-        return None
-    except Exception as e:
-        print(f"Unexpected error: {e}")
-        return None
+class PredictFormData(BaseModel):
+    age: int
+    gender: str
+    localization: str
 
 @app.get('/')
 def read_root():
@@ -56,32 +44,35 @@ def resize_image(image_path, width, height):
     newImage = image.resize((width, height))
     newImage.save("resizedImage.jpg")
 
-@app.post('/predict')
-async def predict(photo: dict):
+@app.post("/predict")
+async def predict(
+                age: int = Form(...), 
+                gender: str = Form(...), 
+                localization: str = Form(...),
+                photo: UploadFile = File(...)):
     try:
-        base64FromUser = photo["data"]["_parts"][0][1]["base64"]
+        image_path = f"images/{photo.filename}"
+
+        print('age: ', age)
+        print('gender: ', gender)
+        print('localization: ', localization)
         print(photo)
+        print('image_path: ', image_path)
 
-        tempImg = None
+        with open(image_path, "wb") as file:
+            contents = await photo.read()
+            file.write(contents) 
 
-        with open("tempImgToSave.jpg", "wb") as file:
-            tempImg = base64.b64decode(base64FromUser)  # actual image in bytes
-            file.write(base64.b64decode(base64FromUser))  # creating a new file and saving it to current directory
-        
-        resize_image("tempImgToSave.jpg", 600, 600)
+        image_to_array(contents, 450, 600)
 
-        image_to_array(tempImg, 600, 600)   # change to whatever width and height of image that is necessary
-
-        if not photo["data"]["_parts"][0][1]["base64"]:
-            return {"msg": "Failed to upload image to the server...", "status": 400}
-
-        return {"msg": "Image received!", "status": 200}
-
+        file.close()
+        return {"file_contents": contents.decode("utf-8", "ignore")}
     except Exception as e:
         print("Error receiving image:", e)
+        return {"error": e}  
 
 def image_to_array(img, width, height):
-    image = Image.open("tempImgToSave.jpg")
+    image = Image.open(io.BytesIO(img))
     
     # image_grayscale = image.convert("L")      # probably want to keep original image color
     # resized_image = image_grayscale.resize((width, height))
@@ -89,6 +80,8 @@ def image_to_array(img, width, height):
     resized_image = image.resize((width, height))   
 
     image_array = np.array(resized_image)
+
+    # print("image_array: ", image_array)
 
     normalized_image = image_array / 255.0  # value depends on the pixel value range
 
